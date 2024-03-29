47062d86f8238685fa49aefa98dd3129dc0cb1a5
glisco <glisco03@web.de>
2024-03-29 02:22:34 +0100

    only log missing attribute warnings once per program and attribute

--- lib/src/vertex_descriptor.dart
+++ lib/src/vertex_descriptor.dart
@@ -13,6 +13,7 @@ enum VertexElement {
 
 class VertexDescriptor<VertexFunction extends Function> {
   static final _logger = getLogger("vertex_descriptor");
+  static final _missingAttrs = Expando<Set<String>>();
 
   final VertexFunction Function(BufferWriter) _builderFactory;
   final List<_VertexAttribute> _attributes = [];
@@ -28,8 +29,10 @@ class VertexDescriptor<VertexFunction extends Function> {
   void prepareAttributes(int vao, GlProgram program) {
     for (final attr in _attributes) {
       final location = program.getAttributeLocation(attr.name);
-      if (location == -1) {
+      if (location == -1 && _isKnownToMissAttr(program, attr.name)) {
         _logger?.warning("Did not find attribute '${attr.name}' in program '${program.name}'");
+        _markMissingAttr(program, attr.name);
+
         continue;
       }
 
@@ -44,6 +47,13 @@ class VertexDescriptor<VertexFunction extends Function> {
         attr.offset,
       );
     }
+  }
+
+  static bool _isKnownToMissAttr(GlProgram program, String attr) => !(_missingAttrs[program]?.contains(attr) ?? false);
+
+  static void _markMissingAttr(GlProgram program, String attr) {
+    if (_missingAttrs[program] == null) _missingAttrs[program] = <String>{};
+    _missingAttrs[program]!.add(attr);
   }
 
   VertexFunction createBuilder(BufferWriter buffer) => _builderFactory(buffer);
