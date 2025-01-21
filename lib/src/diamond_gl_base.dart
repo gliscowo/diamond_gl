f3421120c7c7b1432a2dfca2a7810c572a292e56
glisco <glisco03@web.de>
2025-01-22 00:43:19 +0100

    prevent overriding first logger and expose init flag

--- lib/src/diamond_gl_base.dart
+++ lib/src/diamond_gl_base.dart
@@ -14,8 +14,14 @@ Logger? getLogger(String system) {
   return Logger('${baseLogger!.name}.$system');
 }
 
+bool _initialized = false;
+bool get diamondGLInitialized => _initialized;
+
 void initDiamondGL({Logger? logger}) {
+  if (_initialized) return;
+
   baseLogger = logger;
+  _initialized = true;
 }
 
 extension CString on String {
